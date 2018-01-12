0ceb46ec10402525a6c1ca6521e9399c685f26c6
GitHubMaxwell <maxwell.rediker@gmail.com>
2018-01-11 21:23:49 -0800

    the while loop finally works on to the other questions

--- app.js
+++ app.js
@@ -1,21 +1,94 @@
 'use strict';
 
-var answerOne = prompt("please enter your name");
-    console.log("please answer your name");
-    alert("hello" + answerOne);
-    console.log(answerOne);
+var guestName = prompt('please enter your name');
+console.log('please enter your name');
+alert('Hello ' + guestName + '! Please answer the following 5 questions of my guessing game with a YES or a NO!');
+console.log(guestName);
 
-var answerOne = prompt("please enter your name");
-    console.log("please answer your name");
-    alert("hello" + answerOne);
-    console.log(answerOne);
+var answerOne = prompt('Was I born in the USA?').toUpperCase();
+console.log('Question 1');
+console.log(answerOne);
+
+/*
+var answerTwo = prompt('Has it ever hailed in Saudi Arabia?').toUpperCase;
+console.log('Question 2');
+console.log(answerTwo);
+
+
+var answerThree = prompt('Have I ever been to Antartica?').toUpperCase;
+console.log('Question 3');
+console.log(answerThree);
+
+var answerFour = prompt('Have I been married?').toUpperCase;
+console.log('Question 4');
+console.log(answerFour);
+
+var answerFive = prompt("Do I have Arabian wild dogs as pets?").toUpperCase;
+console.log('Question 5');
+console.log(answerFive); 
+
+*/
+
+if (answerOne === 'YES') {
+
+    alert('Yep! Good job ' + guestName + '! Lets try another one.');
+    console.log(guestName + ' got it right!');
+
+} else if (answerOne === 'NO'){
+    /*
+    alert('Nope! Try again ' + guestName + '!');
+    console.log(guestName + ' got it wrong.');
+    */
     
-var answerOne = prompt("please enter your name");
-    console.log("please answer your name");
-    alert("hello" + answerOne);
-    console.log(answerOne);
+    while (answerOne !== 'YES' || answerOne === 'NO'){
+        alert('Nope! Try again ' + guestName + '!');
+        answerOne = prompt('Was I born in the USA?').toUpperCase();
+        console.log(guestName + ' got it wrong AGAIN.');
 
-var answerOne = prompt("please enter your name");
-    console.log("please answer your name");
-    alert("hello" + answerOne);
-    console.log(answerOne);
+        if (answerOne === 'YES')  {
+            alert('Yep! Good job ' + guestName + '! Lets try another one.');
+        }
+    }
+
+} else {
+    /*
+    alert('Please answer the question with a YES or a NO.');
+    console.log(guestName + ' didn\'t answer the question correctly');
+    */
+    while (answerOne !== 'YES' || answerOne === 'NO') {
+        answerOne = prompt('Was I born in the USA?').toUpperCase();
+    }
+
+}
+   
+if (answerTwo === 'YES') {
+
+    alert('Yep! Good job ' + guestName + '! Lets try another one.');
+    console.log(guestName + ' got it right!');
+
+} else if (answerTwo === 'NO'){
+    /*
+    alert('Nope! Try again ' + guestName + '!');
+    console.log(guestName + ' got it wrong.');
+    */
+    
+    while (answerTwo !== 'YES' || answerTwo === 'NO'){
+        alert('Nope! Try again ' + guestName + '!');
+        answerTwo = prompt('Was I born in the USA?').toUpperCase();
+        console.log(guestName + ' got it wrong AGAIN.');
+
+        if (answerTwo === 'YES')  {
+            alert('Yep! Good job ' + guestName + '! Lets try another one.');
+        }
+    }
+
+} else {
+    /*
+    alert('Please answer the question with a YES or a NO.');
+    console.log(guestName + ' didn\'t answer the question correctly');
+    */
+    while (answerTwo !== 'YES' || answerTwo === 'NO') {
+        answerTwo = prompt('Was I born in the USA?').toUpperCase();
+    }
+
+}
