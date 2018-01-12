1a62de68af306c481700b6053e495a8129a81b2f
GitHubMaxwell <maxwell.rediker@gmail.com>
2018-01-11 19:07:13 -0800

    added html/eslintrc/app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var answerOne = prompt("please enter your name");
+    console.log("please answer your name");
+    alert("hello" + answerOne);
+    console.log(answerOne);
+
+var answerOne = prompt("please enter your name");
+    console.log("please answer your name");
+    alert("hello" + answerOne);
+    console.log(answerOne);
+    
+var answerOne = prompt("please enter your name");
+    console.log("please answer your name");
+    alert("hello" + answerOne);
+    console.log(answerOne);
+
+var answerOne = prompt("please enter your name");
+    console.log("please answer your name");
+    alert("hello" + answerOne);
+    console.log(answerOne);
